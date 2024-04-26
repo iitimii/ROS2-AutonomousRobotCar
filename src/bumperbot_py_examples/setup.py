28fcf7379265abf73f75371421b370b4787b426c
iitimii <timiiowolabi@gmail.com>
2024-04-26 22:10:11 +0100

    update

---
+++ src/bumperbot_py_examples/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'bumperbot_py_examples'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='timi',
+    maintainer_email='timi@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'simple_publisher = bumperbot_py_examples.simple_publisher:main',
+            'simple_subscriber = bumperbot_py_examples.simple_subscriber:main',
+            'simple_parameter = bumperbot_py_examples.simple_parameter:main'
+        ],
+    },
+)
