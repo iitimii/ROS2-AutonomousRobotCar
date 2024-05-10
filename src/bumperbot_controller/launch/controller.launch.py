b3bb053bbc17ee83af03c111f6d5ebf8ebedb201
iitimii <timiiowolabi@gmail.com>
2024-05-10 14:33:21 +0100

    update

--- src/bumperbot_controller/launch/controller.launch.py
+++ src/bumperbot_controller/launch/controller.launch.py
@@ -2,9 +2,20 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
-joint_state_broad
+joint_state_broadcaster_spawner = Node(
+    package='controller_manager',
+    executable='spawner',
+    arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
+)
+
+simpler_controller_spawner = Node(
+    package='controller_manager',
+    executable='spawner',
+    arguments=['simple_velocity_controller', '--controller-manager', '/controller_manager'],
+)
 
 def generate_launch_description():
     return LaunchDescription([
-
+        joint_state_broadcaster_spawner,
+        simpler_controller_spawner,
     ])
